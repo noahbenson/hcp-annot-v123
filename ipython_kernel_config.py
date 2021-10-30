feeda6a76a98c72c5ecc942591d3cfa931441e80
Noah C. Benson <n@nben.net>
2021-10-30 06:55:04 -0700

    moved

--- ipython_kernel_config.py
+++
@@ -1,4 +0,0 @@
-# Automatic %matplotlib widgets (per https://stackoverflow.com/questions/21176731/automatically-run-matplotlib-inline-in-ipython-notebook)
-c = get_config()
-c.InteractiveShellApp.matplotlib = "widget"
-
